42c3635dfbe23c27c33a61cf8fb3c0df83414ac9
Guilherme Tinem <guizstinem@gmail.com>
2020-07-23 21:21:49 -0300

    :zap: Desafio Concluido -> Aplicado as regras de negocios, aplicado validacao dos dados da request

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,21 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+/**
+ * * Regras de negócios para aplicar:
+ *
+ * TODO [X] -> Checar se o valor passado em "value" é um number
+ * TODO [X] -> Checar se type é income ou outcome
+ * TODO [X] -> Quando valor é outcome, checar se o valor não ultrapassa o total
+ *
+ */
+
+interface TransactionRequest {
+  title: string;
+  value: number;
+  type: "income" | "outcome";
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +23,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: TransactionRequest): Transaction {
+
+    if (!Number.isInteger(value)) {
+      throw Error('Value passed must be a number');
+    }
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Type passed not allowed');
+    }
+
+    if (type == 'outcome') {
+      const { total } = this.transactionsRepository.getBalance();
+      if ( value > total) {
+        throw Error('Value do not be higher than total value');
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({ title, type, value })
+
+    return transaction
   }
 }
 
